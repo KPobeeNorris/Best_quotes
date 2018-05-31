4b337dbe1c82ce84c68c85db03327458d0248e8e
Kath Pobee-Norris <kpobeenorris@gmail.com>
2018-05-31 16:22:47 +0100

    Created db which uses json files. Introduced create and update methods and updated views to render these appropriately

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,36 @@
 class QuotesController < Rulers::Controller
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels",
+      "attribution" => "Me"
+    }
+
+    m = FileModel.create(attrs)
+    render :quote, :obj => m
+  end
+
+  def update_quote
+
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "FINAL TEST",
+      "attribution" => "KATH"
+    }
+
+    id = "10"
+    FileModel.save(attrs, id)
+
+    m = Rulers::Model::FileModel.find(10)
+    render :quote, :obj => m
+  end
+
   def a_quote
     render :a_quote, noun: :winking
   end
@@ -9,5 +41,10 @@ class QuotesController < Rulers::Controller
 
   def this_controller_name
     render :this_controller_name
+  end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
   end
 end
